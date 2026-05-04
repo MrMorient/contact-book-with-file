2073fef3ffef0f5357493dd3ed65ffd9acb3281f
Desiree <desi098rivera@gmail.com>
2026-05-04 19:34:02 -0400

    Represents a Contact

---
+++ Contact.java
@@ -0,0 +1,20 @@
+package app.ContactBook;
+
+class Contact {
+    String name;
+    String phone;
+    String email;
+
+    Contact(String name, String phone, String email) {
+        this.name = name;
+        this.phone = phone;
+        this.email = email;
+    }
+
+    void display() {
+        System.out.println("Name: " + name);
+        System.out.println("Phone: " + phone);
+        System.out.println("Email: " + email);
+        System.out.println("----------------------");
+    }
+}
